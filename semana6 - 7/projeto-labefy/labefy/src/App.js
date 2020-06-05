97f7224be9c256a178f914a0ed73cbf7010eb512
deborahanalistati@gmail.com <deborahanalistati@gmail.com>
2020-06-05 15:28:12 -0300

    Implementação Criar Playlist

---
+++ semana6 - 7/projeto-labefy/labefy/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import CreatePlaylist from "./Components/CreatePlaylist";
+
+function App() {
+  return <CreatePlaylist />;
+}
+
+export default App;
---
+++ semana6 - 7/projeto-labefy/labefy/src/Components/CreatePlaylist.js
@@ -0,0 +1,59 @@
+import React from "react";
+import axios from "axios";
+
+/* Falta a verificação de nome igual */
+
+class CreatePlaylist extends React.Component {
+  state = {
+    playlists: ["texte1", "teste2"],
+    inputPlaylist: "",
+  };
+
+  //Método estado atual input
+  handlePlaylistChange = (event) => {
+    this.setState({ inputPlaylist: event.target.value });
+  };
+
+  //Método Criar Playlist
+  createPlaylist = () => {
+    const axiosConfig = {
+      headers: {
+        Authorization: "Deborah-Vieira-mello",
+      },
+    };
+
+    const body = {
+      name: this.state.inputPlaylist,
+    };
+
+    axios
+      .post(
+        "https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists",
+        body,
+        axiosConfig
+      )
+      .then((response) => {
+        alert("Musica criada com sucesso");
+        this.setState({ inputPlaylist: "" });
+      })
+      .catch((erro) => {
+        alert("Falha ao criar a playlist");
+      });
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <input
+          type="text"
+          placeholder="Nome da playlist"
+          value={this.state.inputPlaylist}
+          onChange={this.handlePlaylistChange}
+        />
+        <button onClick={this.createPlaylist}>Criar Playlist</button>
+      </div>
+    );
+  }
+}
+
+export default CreatePlaylist;
