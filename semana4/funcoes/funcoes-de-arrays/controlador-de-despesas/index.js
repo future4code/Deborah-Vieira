a1c021dc674b0fc8d8e6699758a34bcd406c8f84
Deborah Vieira <deborahanalistati@gmail.com>
2020-08-06 16:54:09 -0300

    “funcoes-arrays”

---
+++ semana4/funcoes/funcoes-de-arrays/controlador-de-despesas/index.js
@@ -0,0 +1,23 @@
+let arrayDespesas = []
+
+function addDespesas(){
+    //pegando o valor dos campos
+    const valorDespesa = document.getElementById('valor-despesa')
+    const tipoDespesa = document.getElementById("casa", 'viagem', 'escritorio')
+    const descricaoDespesa = document.getElementById('descricao')
+
+    //objeto de despesa
+    const novaDespesa = {
+        valor = valorDespesa.value,
+        tipo = tipoDespesa.value,
+        descricao = descricaoDespesa.value
+    }
+
+
+    arrayPosts.push(novaDespesa)
+    //limpando os campos
+    valorDespesa.value =""
+    tipoDespesa.value =""
+    descricaoDespesa.value =""
+    console.log(arrayDespesas)
+}
