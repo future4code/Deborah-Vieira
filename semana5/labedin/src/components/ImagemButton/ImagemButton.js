5bf8a6cd93e4d716f859f97725f041f597148dc6
Deborah Vieira <deborahanalistati@gmail.com>
2020-08-11 14:45:33 -0300

    “Componentes”

--- semana5/labedin/src/components/ImagemButton/ImagemButton.js
+++ semana5/labedin/src/components/ImagemButton/ImagemButton.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './ImagemButton.css'
 
 function ImagemButton(props) {
+    /*está recebendo como props que é um objeto vindo do Pai do elemento 
+    a imagem e o texto*/
     return (
         <div className="image-button-container">
             <img src={ props.imagem }/>
