f339dce6bcd6f55119ab2ec7a0aeb3483e193853
Deborah Vieira <deborahanalistati@gmail.com>
2020-08-18 17:27:23 -0300

    “Ciclos-de-Vida-React”

---
+++ semana6/Listas-Tarefas/build/precache-manifest.38158316204fdb640493bdc680794f1c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7414b90a7a83b94b76917facd44ff184",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dfc09d1f2125468708d2",
+    "url": "/static/css/main.66d6f871.chunk.css"
+  },
+  {
+    "revision": "078d8750ac3017505296",
+    "url": "/static/js/2.29e9a946.chunk.js"
+  },
+  {
+    "revision": "dfc09d1f2125468708d2",
+    "url": "/static/js/main.38e26b08.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
